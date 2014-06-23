203f77cfff0f02c871b973403a40312fdfd2ae4a
eladamrani <eladamrani@gmail.com>
2014-06-23 22:36:51 +0300

    fdfdfdf

--- vendor/assets/javascripts/spree/frontend/all.js
+++ vendor/assets/javascripts/spree/frontend/all.js
@@ -10,31 +10,3 @@
 //= require spree/frontend
 
 //= require_tree .
-$( document ).ready(function() {
-	//Header add class to nav bar (pos_0)
-	var count = 0;
-	$( "#nav-bar li" ).each(function() {
-	 	$(this).addClass('pos_' + count );
-	 	count++;
-	}); 
-
-	//Header change my account to he
-	$('#nav-bar .pos_0 a ').text('התחבר  / ');
-	$('#nav-bar .pos_1 a ').text('הרשם');
-
-	$('input#keywords').attr("placeholder", "");
-});
-
-
-$(window).load(function() {
-		//Header change text Cart
-		$('#link-to-cart a .amount').remove();
-		var old_text_cart = $('a.cart-info.full').text();
-		var new_text_cart = old_text_cart.replace("Cart:", "עגלת קניות");
-		$('a.cart-info.full').text(new_text_cart);
-});
-
-
-
-
-
