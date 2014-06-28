94f2f09f274fedbd73909336ad1c883ffb11abf4
eladamrani <eladamrani@gmail.com>
2014-06-28 12:17:42 +0300

    dddd

--- vendor/assets/javascripts/spree/frontend/all.js
+++ vendor/assets/javascripts/spree/frontend/all.js
@@ -41,13 +41,13 @@ $( document ).ready(function() {
 
 $(window).load(function() {
 			//Header change text Cart
-		$('#link-to-cart a .amount').remove();
+		$('nav #main-nav-bar li#link-to-cart a .amount').remove();
 		var old_text_cart = $('a.cart-info.full').text();
-		var new_text_cart = old_text_cart.replace("Cart", "עגלת קניות");
+		var new_text_cart = old_text_cart.replace("Cart:", "עגלת קניות");
 		$('a.cart-info.full').text(new_text_cart);
 
 		var value = $('.search-results-title').text(); 
-		value = value.replace("Search results for", " : תוצאות החיפוש שלך ל");
+		value = value.replace("Search results for", "תוצאות החיפוש שלך ל : ");
 		$('.search-results-title').text(value);
 });
 
