b47982a56f677151b665d96d1042a7d2ecaf9ea9
Daniel <danielt69@gmail.com>
2014-06-29 23:35:10 +0300

    עגלת קניות

--- vendor/assets/javascripts/spree/frontend/all.js
+++ vendor/assets/javascripts/spree/frontend/all.js
@@ -45,10 +45,10 @@ $( document ).ready(function() {
 
 $(window).load(function() {
 			//Header change text Cart
-		$('nav #main-nav-bar li#link-to-cart a .amount').remove();
-		var old_text_cart = $('a.cart-info.full').text();
-		var new_text_cart = old_text_cart.replace("Cart:", "עגלת קניות");
-		$('a.cart-info.full').text(new_text_cart);
+		// $('nav #main-nav-bar li#link-to-cart a .amount').remove();
+		// var old_text_cart = $('a.cart-info.full').text();
+		// var new_text_cart = old_text_cart.replace("Cart:", "עגלת קניות");
+		// $('a.cart-info.full').text(new_text_cart);
 
 		var value = $('.search-results-title').text(); 
 		value = value.replace("Search results for", "תוצאות החיפוש שלך ל : ");
