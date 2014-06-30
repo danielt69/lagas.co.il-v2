75041c4795370260d4e1ce286c22dfe39fa6050a
Daniel <danielt69@gmail.com>
2014-06-30 23:00:18 +0300

    added newsticker element

--- vendor/assets/javascripts/spree/frontend/all.js
+++ vendor/assets/javascripts/spree/frontend/all.js
@@ -102,8 +102,11 @@ $(document).ready(function() {
 // temp
 $(document).ready(function() {
     $('div[data-hook="product_price"]').prepend('<div class="temp">להזמנות חייגו עכשיו: 052-457-2703 <br />מייל: otn.moas@gmail.com<br /> *בקרוב: הזמנות אונליין באתר!</div>');
+    $("input#contact_us_contact_submit").val('שלח פנייה');
 });
 
+// newsTicker
+$('.newsticker').newsTicker();
 
 
 //= require spree/frontend/spree_contact_us
